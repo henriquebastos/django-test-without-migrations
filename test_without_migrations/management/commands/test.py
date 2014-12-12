47fa6f75651709f2408fcd51926909b9f3a73a50
Henrique Bastos <henrique@bastos.net>
2014-12-12 16:01:01 -0300

    Implement command extension

---
+++ test_without_migrations/management/commands/test.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from optparse import make_option
+from django.core.management.commands.test import Command as TestCommand
+
+
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+
+class Command(TestCommand):
+    option_list = TestCommand.option_list + (
+        make_option('--nomigrations',
+            action='store_true', dest='interactive', default=False,
+            help='Tells Django to NOT use migrations and create all tables directly.'),
+    )
+
+    def handle(self, *test_labels, **options):
+        from django.conf import settings
+
+        if options['nomigrations']:
+            settings.MIGRATION_MODULES = DisableMigrations()
+
+        super(Command, self).handle(*test_labels, **options)
